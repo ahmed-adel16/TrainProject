9b017e5c86dde1a1939a69de429bbd0ccadf863f
ahmed-adel16 <ahmed.be1556@gmail.com>
2024-04-21 00:36:08 +0200

    database connection

--- src/Train/Employee.java
+++ src/Train/Employee.java
@@ -1,35 +1,87 @@
 
 package Train;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
+import java.sql.*;
 
 
 public class Employee extends Person {
-    private static int nextEmployeeId = 1; // Static variable to generate unique employee IDs
-    private String employeeId; // Unique ID for the employee
+    private String employeeUID; // Unique ID for the employee
+    private String password;
+
     private static HashMap <String, String> employeeCredentials = new HashMap<>();
+    static Connection c = null;
     // Constructor to initialize employee attributes
     public Employee(String name, String email, int age, String tel, String password) {
         super(name, email, age, tel); // Call to parent class constructor
-        this.employeeId = "EMP" + nextEmployeeId++; // Generate employee ID
-        registerEmployee(name, email, age, tel, password);
+        this.employeeUID = "EMP"; // Generate employee ID    
+        this.password = password;
     }
-    public static boolean authenticateEmployee(String email, String password) {
-        String storedPassword = employeeCredentials.get(email);
-        return storedPassword != null && storedPassword.equals(password);
+
+    public String getEmployeeUID() {
+        return employeeUID;
     }
-        
-    public static boolean registerEmployee(String name, String email, int age, String tel, String password) {
-            // Check if the email is already in use
-            if (employeeCredentials.containsKey(email)) {
-                System.out.println("Email is already registered.");
-                return false;
+    
+    public String getPassword() {
+        return doHashing(password);
+    }
+    
+    public static void main(String args[]) throws ClassNotFoundException, SQLException{
+        Employee e2 = new Employee("Ahmed", "Ahmed11222@gmail.com", 20, "01029957328", "zaghloul22");
+        e2.saveData();
+        Employee e3 = new Employee("Zaghloul", "zaghloul222@gmail.com", 19, "01045822345", "zaghloul22");
+        e3.saveData();
+
+
+
+    }
+    
+    public void saveData() throws ClassNotFoundException, SQLException{
+            PreparedStatement ps = null;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Connected");
+            String sql = "INSERT INTO Employee (Name, Age, Email, Password) VALUES (?, ?, ?, ?)";
+            ps = c.prepareStatement(sql);
+            
+            ps.setString(1, this.getName());
+            ps.setInt(2, this.getAge());
+            ps.setString(3, this.getEmail());
+            ps.setString(4, this.getPassword());
+            ps.executeUpdate();
+            ps.close();
+            c.close();
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    
+    public static String doHashing(String password) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+            messageDigest.update(password.getBytes());
+
+            byte[] resultByteArray = messageDigest.digest();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : resultByteArray) {
+                sb.append(String.format("%02x", b));
             }
-            // Add the new employee to the credentials map
-            employeeCredentials.put(email, password);
-            System.out.println("Employee registered successfully.");
-            return true;
+
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
+    
 }
 
 
